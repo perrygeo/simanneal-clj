09c48d374278bb3d7568da8b0fcced2cecf404f7
Matthew Perry <perrygeo@gmail.com>
2018-05-11 21:59:30 -0600

    working implementation of TSP

--- src/simanneal/examples.clj
+++ src/simanneal/examples.clj
@@ -38,7 +38,7 @@
 ; (defn score
 ;   "Given the state, calculate how well it meets the objective.
 ;   Lowest score is best; minimize
-  
+
 ;   In this case, take the negative sum of the first half
 ;   ie optimize so that greater values sort to the first half"
 ;   [state]
@@ -50,28 +50,43 @@
 ;; (run-sa initial-state move score temps)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; demo
-;;
-;; sort bigger things into the first half of the vector
-;;
-;; Not useful but easy to debug
-(def initial-state (into [] (range 1000)))
+; Travelling Salesman Problem
+;
+; Simulating nodes of a graph for which we'll calculate
+; the shortest tour visiting all points
+
+(defn random-point
+  "Random 2D point vector"
+  []
+  [(rand) (rand)])
+
+(def initial-state
+  (into []
+        ; Vector of 2D point vectors.
+        (take 49 (repeatedly random-point))))
 
 (defn move
-  "Return a new state with a small modification"
-  [state]
-  (let [idx1 (rand-int (count state))
-        idx2 (rand-int (count state))]
-    (switch-elements state idx1 idx2)))
+  "Return a new tour with a small modification"
+  [tour]
+  (let [point1 (rand-int (count tour))
+        point2 (rand-int (count tour))]
+    (switch-elements tour point1 point2)))
+
+(defn distance
+  "Take two 2D point vectors and calculate euclidean distance"
+  [[x1 y1] [x2 y2]]
+  (let [dx (- x2 x1)
+        dy (- y2 y1)]
+    (Math/sqrt (+ (* dx dx) (* dy dy)))))
 
 (defn score
   "Given the state, calculate how well it meets the objective.
   Lowest score is best; minimize
-  
-  In this case, take the negative sum of the first half
-  ie optimize so that greater values sort to the first half"
-  [state]
-  (* -1 (r/fold + (first-half state))))
+  Calculate the total distance along the route"
+  [tour]
+  (reduce
+   + (for [[pt1 pt2] (partition 2 1 tour)]
+       (distance pt1 pt2))))
 
 (def temps
-  (make-temperature-seq 2500.0 0.1 50000))
+  (make-temperature-seq 25.0 0.1 500000))
